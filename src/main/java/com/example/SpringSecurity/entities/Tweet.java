d4fde5359687ccef1b26c0fb28828fd44f968ff1
dduartess <dandandsoares@gmail.com>
2026-01-09 16:20:40 -0300

    Feat: Create entities User, Role, Tweet and your atibuts and JPA relations

---
+++ src/main/java/com/example/SpringSecurity/entities/Tweet.java
@@ -0,0 +1,52 @@
+package com.example.SpringSecurity.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
+    private Long tweetId;
+    @ManyToOne
+    private User user;
+    private String content;
+    @CreationTimestamp
+    private Instant creationTimeStamp;
+
+    public Long getTweetId() {
+        return tweetId;
+    }
+
+    public void setTweetId(Long tweetId) {
+        this.tweetId = tweetId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(Instant creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+}
